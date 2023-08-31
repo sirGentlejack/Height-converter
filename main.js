11b320df8e8e12207bcfe96919158fd801109132
sirGentlejack <fagbemiilemobayo@gmail.com>
2023-08-31 20:31:01 +0100

    adding js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+document.getElementById("submit").addEventListener("click", heightConventer);
+
+function heightConventer() {
+  var feet = parseInt(document.getElementById("feet").value *12);
+  var inches = parseInt(document.getElementById("inches").value);
+  
+  var cm =(feet + inches)* 2.54;
+  var n = cm.toFixed(0);
+  
+  
+  
+  document.getElementById("result").innerHTML = n;
+}
